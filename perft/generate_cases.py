500e24ef07a33f06d8bb8c0780d2ceef12835ad0
Gideon Grinberg <grinberg1165@gmail.com>
2025-07-08 23:11:18 -0400

    Max depth of 4 for all test cases

--- perft/generate_cases.py
+++ perft/generate_cases.py
@@ -10,7 +10,7 @@ with open(path / "perftsuite.epd") as f:
         fen = case[0]
         expected = [1]
         for j, num in enumerate(case[1:]):
-            if j >= 5:
+            if j >= 4:
                 break
             expected.append(int(num.split(" ")[1].strip()))
 
@@ -27,13 +27,13 @@ other_cases = [
     {
         "name": "kiwipete",
         "fen": "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ",
-        "depth": 6,
+        "depth": 4,
         "expected": [1, 48, 2039, 97862, 4085603, 193690690, 8031647685],
     },
     {
         "name": "CP Wiki #5",
         "fen": "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
-        "depth": 5,
+        "depth": 4,
         "expected": [1, 48, 2039, 97862, 4085603],
     },
 ]
