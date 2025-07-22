d2a447ff0e20943654d43cb6b48ae01dae42eb5b
Gideon Grinberg <grinberg1165@gmail.com>
2025-07-22 18:21:37 -0400

    Fix flags in build script

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 PLATFORM=$(echo "$(uname -s)-$(uname -m)" | tr '[:upper:]' '[:lower:]')
 BUILD_DIR="build-$PLATFORM"
 DIST_DIR="dist-$PLATFORM"
 BIN_DIR="gce-$PLATFORM"
 mkdir -p "$BUILD_DIR"
-cmake -S . -B "$BUILD_DIR"
+cmake -S . -B "$BUILD_DIR" -DENABLE_PACKAGING=ON
 cmake --build "$BUILD_DIR"
 cmake --install "$BUILD_DIR" --prefix "$DIST_DIR"
 mv "$DIST_DIR/bin" "$BIN_DIR"
